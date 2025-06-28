d0b41e7089d44c72051cce7a65b3b9b43e2abf9e
0xfurai <yevhen.piotrovskyi@gmail.com>
2025-06-28 12:43:08 -0400

    feat: add end-to-end testing for login and registration
    - Introduced new end-to-end tests for user login and registration using Playwright.
    - Updated package.json to include an e2e script for running Playwright tests.
    - Commented out existing test cases for clarity and focus on new functionality.

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,44 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+test('Error when login with invalid credentials', async ({ page }) => {
+  await page.goto('http://localhost:8383/login');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('test@test.com');
+  await page.getByRole('textbox', { name: 'Email' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('TestPassword12345!');
+  await page.getByRole('button', { name: 'Login' }).click();
+  const error = await page.getByText('Errorinvalid credentials');
+  await expect(error).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Register new user', async ({ page }) => {
+  await page.goto('http://localhost:8383/register');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('test@test.com');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill('TestPassword1234!');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).press('Tab');
+  await page.getByRole('textbox', { name: 'Confirm Password' }).fill('TestPassword1234!');
+  await page.getByRole('button', { name: 'Create' }).click();
+
+  await expect(page).toHaveURL('http://localhost:8383/monitors');
 });
